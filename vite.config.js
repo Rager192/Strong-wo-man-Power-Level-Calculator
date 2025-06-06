5880167e435287fffd1e4659a1c5e0491967339e
Rager192 <160792848+Rager192@users.noreply.github.com>
2025-06-07 00:27:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'], // <— hier!
   },
-})
-
+  esbuild: {
+    jsx: 'automatic',
+    include: /\.js$/, // <-- sagt Vite: auch .js als JSX behandeln
+  },
+});
