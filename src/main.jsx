b5634d5bf15ba97ef0c89c7a9270d212bf361c96
Rager192 <160792848+Rager192@users.noreply.github.com>
2025-06-08 11:20:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import './index.css';
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Importiere deine Haupt-App-Komponente
+import './index.css'; // Importiere dein globales CSS mit Tailwind-Direktiven
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
